d872169be537a7e0ed2eb659b95986fff7f8fd27
Gaurav Singh <gauravcool.680@gmail.com>
2025-12-13 19:53:55 +0530

    Added new pytrhon problem solution

---
+++ Division.py
@@ -0,0 +1,8 @@
+#QUESTION
+#The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+#The first line contains the integer division of the two numbers (//).
+
+a = int(input("input first integer: "))
+b = int(input("input second integer: "))
+print(a // b)
+print(a / b)
